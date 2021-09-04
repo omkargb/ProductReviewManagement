39b2c670756ca5129bdabd67869f6aa7a47448a0
omkargb <omkargb107@gmail.com>
2021-09-04 08:47:06 +0530

    [omkargb] Add : program files added and Products details added to list

---
+++ ProductReviewProgram/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review managament program ");
+
+            List<ProductReview> Product;
+            Product = new List<ProductReview>
+            {
+                new ProductReview() { ProductId = 10, UserId = 13, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 70, UserId = 16, Rating = 2, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 30, UserId = 111, Rating = 1, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 80, UserId = 167, Rating = 5, Review = "good", isLike = true },
+                new ProductReview() { ProductId = 20, UserId = 13, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 60, UserId = 15, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 10, UserId = 19, Rating = 1, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 80, UserId = 12, Rating = 2, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 50, UserId = 16, Rating = 3, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 40, UserId = 111, Rating = 2, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 100, UserId = 178, Rating = 1, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 30, UserId = 123, Rating = 3, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 90, UserId = 18, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 20, UserId = 11, Rating = 5, Review = "good", isLike = true },
+                new ProductReview() { ProductId = 70, UserId = 16, Rating = 5, Review = "good", isLike = true },
+                new ProductReview() { ProductId = 10, UserId = 19, Rating = 5, Review = "good", isLike = true },
+                new ProductReview() { ProductId = 90, UserId = 12, Rating = 2, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 40, UserId = 15, Rating = 1, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 60, UserId = 11, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 20, UserId = 15, Rating = 1, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 80, UserId = 19, Rating = 5, Review = "good", isLike = true },
+                new ProductReview() { ProductId = 30, UserId = 19, Rating = 5, Review = "good", isLike = true },
+                new ProductReview() { ProductId = 50, UserId = 11, Rating = 2, Review = "bad", isLike = false },
+                new ProductReview() { ProductId = 60, UserId = 25, Rating = 3, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 90, UserId = 19, Rating = 1, Review = "bad", isLike = false }
+            };
+
+        }
+    }
+}
